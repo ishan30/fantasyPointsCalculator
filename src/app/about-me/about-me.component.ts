96e1025e84b1958c2a370f9275e0554a7aca0da2
apple <apple@apples-MacBook-Air.local>
2020-10-30 18:58:51 +0530

    adding animations

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -10,6 +10,18 @@ export class AboutMeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let current = 1; // keeps track of the current div
+//     const height = ('.roles').height(); // the height of the roles div
+//     const numberDivs = ('.roles').children().length; // the number of children of the roles div
+//     const first = ('.roles div:nth-child(1)'); // the first div nested in roles div
+//     setInterval(function() {
+//     const number = current * -height;
+//     first.css('margin-top', number + 'px');
+//     if (current === numberDivs) {
+//         first.css('margin-top', '0px');
+//         current = 1;
+//     } else { current++; }
+// }, 2000);
   }
 
 }
